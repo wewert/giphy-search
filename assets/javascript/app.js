f119232b299337e260e7853a83543d80c1c39994
Ken Lee <wewert@gmail.com>
2018-01-14 22:33:07 -0700

    fixed pause play feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,7 +25,6 @@ function renderButtons() {
 
 $(document).on("click", "button", function() {
   var gif = $(this).attr("data-name");
-console.log(gif);
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=yLqbUMyxlxr22VOQW7oJ60ySj4sLjt7O&limit=10";
 
@@ -33,31 +32,27 @@ console.log(gif);
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        console.log(response);
           var results = response.data;
           for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
             var gifImage = $("<img>");
 
-            gifImage.attr("src", results[i].images.fixed_height.url);
-            // gifImage.attr("src",
-            // results[i].images.fixed_height_still.url);
+            // gifImage.attr("src", results[i].images.fixed_height.url);
+            gifImage.attr("src",
+            results[i].images.fixed_height_still.url);
             gifImage.attr("data-still", results[i].images.fixed_height_still.url);
             gifImage.attr("data-animate", results[i].images.fixed_height.url);
-            gifImage.attr(JSON.stringify('data-state="still"'));
-
-             // + ' ' + 'data-state= + "still"' + ' ' + 'class= + "gif"';
+            gifImage.attr("data-still", "still");
+            gifImage.attr("class", "gif");
 
             gifDiv.append(gifImage);
 
             $("#gifs-appear-here").prepend(gifDiv);
 
-            $(document).on("click", "gif", function() {
-              // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            $(document).on("click", "img.gif", function() {
+
               var state = $(this).attr("data-state");
-              // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-              // Then, set the image's data-state to animate
-              // Else set src to the data-still value
+
               if (state === "still") {
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("data-state", "animate");
@@ -68,7 +63,4 @@ console.log(gif);
             });
           }
         });
-
-    // gifImage.attr("src", results[i].images.fixed_height_still.url);
-    // $('.gif').giplayer();
 });
